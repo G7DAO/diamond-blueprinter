becd222725ca33cfd436ac013722034a4c059210
Evgeny Tsvigun <utgarda@gmail.com>
2023-03-17 21:03:36 +0300

    draft: simple backpack #1

---
+++ contracts/facets/BackpackSystem.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity ^0.8.0;
+
+/**
+    @notice simple backpack - a set of items, each item is either present or not
+    */
+contract BackpackSystem {
+    bytes32 constant DIAMOND_STORAGE_POSITION = keccak256("backpack.storage");
+
+    struct BackpackState {
+        mapping(uint256 => bool) backpack;
+    }
+
+    event Put(uint256 item);
+    event Drop(uint256 item);
+
+
+    function diamondStorage() internal pure returns (BackpackState storage ds) {
+        bytes32 position = DIAMOND_STORAGE_POSITION;
+        assembly {
+            ds.slot := position
+        }
+    }
+
+    function put(uint256 item) external {
+        BackpackState storage backpackState = diamondStorage();
+        if(!backpackState.backpack[item])
+            emit Put(item);
+        backpackState.backpack[item] = true;
+    }
+
+    function drop(uint256 item) external {
+        BackpackState storage backpackState = diamondStorage();
+        if(backpackState.backpack[item])
+            emit Drop(item);
+        backpackState.backpack[item] = false;
+    }
+
+    function contains(uint256 item) external view returns (bool) {
+        BackpackState storage backpackState = diamondStorage();
+        return backpackState.backpack[item];
+    }
+
+    function supportsInterface(bytes4 _interfaceID) external pure returns (bool) {
+        return _interfaceID == interfaceId();
+    }
+
+    function interfaceId() private pure returns (bytes4){
+        return this.put.selector ^ this.drop.selector ^ this.contains.selector;
+    }
+}
