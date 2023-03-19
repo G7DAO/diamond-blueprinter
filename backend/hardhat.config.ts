14e967dc84fd1e33bb7c952f8b53de6bd1cd681b
Evgeny Tsvigun <utgarda@gmail.com>
2023-03-19 21:04:31 +0300

    Kimberlite contract #3

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require('dotenv').config();
+//require("@nomiclabs/hardhat-ethers");
+//require("@nomiclabs/hardhat-etherscan");
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+    solidity: "0.8.18",
+    networks: {
+        polygon: {
+            url: process.env.ALCHEMY_URL,
+            accounts: {
+                mnemonic: process.env.MNEMONIC_MAINNET,
+            },
+        },
+    }
 };
 
 export default config;
