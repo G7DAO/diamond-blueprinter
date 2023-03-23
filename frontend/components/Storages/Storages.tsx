05faa8fb7fc8446ed3d007feb2c796e08d18c89b
poludnev <poludnev.roman@gmail.com>
2023-03-23 13:15:30 +0300

    Added storages block

--- frontend/components/Storages/Storages.tsx
+++ frontend/components/Storages/Storages.tsx
@@ -2,25 +2,8 @@ import { IStoragesProps } from './StoragesProps';
 import styles from './Storages.module.scss';
 import { Storage } from './Storage/Storage';
 import { IStoragaData } from '@/types';
-export const Storages: React.FC<IStoragesProps> = () => {
-  const storages: IStoragaData[] = [
-    {
-      logo: '📦',
-      name: 'Smashcraft.Backpack.Storage',
-      description: '💠 Simple(Movement)',
-    },
-    {
-      logo: '🧭',
-      name: 'Smashcraft.Worldmap.Storage',
-      description: '💠 Simple(Movement)',
-    },
-    {
-      logo: '📦',
-      name: 'Smashcraft.CraftReceipt.Storage',
-      description: '💠 Simple(Movement)',
-    },
-  ];
-  
+export const Storages: React.FC<IStoragesProps> = ({ storages }) => {
+
   return (
     <div className={styles.container}>
       <h2 className={styles.h2}>Storages</h2>
--- frontend/components/Storages/StoragesProps.ts
+++ frontend/components/Storages/StoragesProps.ts
@@ -1,3 +1,6 @@
+import { IStoragaData } from '@/types';
+
 export interface IStoragesProps {
   children?: React.ReactNode;
+  storages: IStoragaData[];
 }
