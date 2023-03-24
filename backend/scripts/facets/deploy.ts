8d5cc416b0f1d0dce8b7f002ed4e992d8bc2ec48
elizavetta <elizavetta.net@gmail.com>
2023-03-25 00:32:22 +0300

    meta json prettified

--- backend/scripts/facets/deploy.ts
+++ backend/scripts/facets/deploy.ts
@@ -2,24 +2,16 @@ import { ethers } from "hardhat";
 import { BaseContract } from "ethers";
 import fs from 'fs/promises';
 
-const groupName = "Characters"
-const systemName = "TrivialCharacterSystem"
-const storageKey = "character.storage"
-const storageContents  = `struct AliveState {
+// this constants are used to describe metadata
+// ATTN! Please change them before running deploy script
+const GROUP = "Characters"
+const NAME = "TrivialCharacterSystem"
+const STORAGEKEY = "smashcraft.character.storage"
+const STORAGECONTETS  = `
+  struct AliveState {
     mapping(uint256 => bool) alive;
   }
-
-  function diamondStorage() internal pure returns (AliveState storage ds) {
-    bytes32 position = DIAMOND_STORAGE_POSITION;
-    assembly {
-        ds.slot := position
-    }
-  }
-
-  function _isAlive(uint256 id) internal view returns (bool){
-    AliveState storage state = diamondStorage();
-    return state.alive[id];
-  }`
+`
 
 async function deploySystem(systemName: string): Promise<BaseContract> {
   const System = await ethers.getContractFactory(systemName)
@@ -31,19 +23,18 @@ async function deploySystem(systemName: string): Promise<BaseContract> {
   let resultJson = {
     address: system.address,
     explorer_url : `https://polygonscan.com/address/${system.address}#code`,
-    name: systemName,
-    group: groupName,
-    [storageKey]: storageContents
+    name: NAME,
+    group: GROUP,
+    [STORAGEKEY]: STORAGECONTETS
   }
 
   await fs.writeFile(
     'cache/registry_metadata_saved.json',
-    JSON.stringify(resultJson)
+    JSON.stringify(resultJson, null, 4)
   );
 
   return system
 }
-
 
 
 if (require.main === module) {
